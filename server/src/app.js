30edcf2d23e9ec577c1c57a692cc6034f94d01b3
Niyati Kailkhura <niyatikailkhura@gmail.com>
2026-09-20 13:50:46 +0530

    Add Women FIR feature

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const evidenceRoutes = require("./routes/evidenceRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
 const signatureRoutes = require("./routes/signatureRoutes");
 const custodyRoutes = require("./routes/custodyRoutes");
-const courtRoutes = require("./routes/courtRoutes");
+
 
 const app = express();
 
